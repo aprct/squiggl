893ec09939f165025708b201e4853ea91736b633
aprct <weekoldsushi@gmail.com>
2018-10-18 10:55:32 -0700

    fix site

---
+++ docs/precache-manifest.fa79e7b8f1225a14eb95f99f215dc9e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "27e448483b5f7fc82f68",
+    "url": "/squiggl/static/js/runtime~main.27e44848.js"
+  },
+  {
+    "revision": "0e9ea0f4059084e302f5",
+    "url": "/squiggl/static/js/main.0e9ea0f4.chunk.js"
+  },
+  {
+    "revision": "64bbf5dcf17cd3facd85",
+    "url": "/squiggl/static/js/1.64bbf5dc.chunk.js"
+  },
+  {
+    "revision": "0e9ea0f4059084e302f5",
+    "url": "/squiggl/static/css/main.0759feed.chunk.css"
+  },
+  {
+    "revision": "627cae8545a0c38970c26b1678ff552d",
+    "url": "/squiggl/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a04666badaecd6f17abbbd27eb511a67.js"
+  "/squiggl/precache-manifest.fa79e7b8f1225a14eb95f99f215dc9e2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/squiggl/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
